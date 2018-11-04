5e919eab95ebed61251fda349c1ca3b84abe5039
nicholasbarger <nicholas@nicholasbarger.com>
2018-11-04 09:03:52 -0600

    Worked on gravatar image for account

--- src/app/account/profile-page/profile-page.component.ts
+++ src/app/account/profile-page/profile-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { Md5 } from 'ts-md5';
 
 @Component({
   selector: 'app-profile-page',
@@ -8,9 +9,16 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class ProfilePageComponent implements OnInit {
 
+  profileImage: string;
+
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    var user = this.authService.getUser();
+
+    // setup gravatar
+    var hashedEmail = Md5.hashStr(user.email.toLowerCase());
+    this.profileImage = `https://www.gravatar.com/avatar/${hashedEmail}?d=mp&s=200`;
   }
 
   logout(): void {
--- src/app/common/navigation/navigation.component.ts
+++ src/app/common/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { Md5 } from 'ts-md5';
 
 @Component({
   selector: 'navigation',
@@ -8,9 +9,15 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class NavigationComponent implements OnInit {
 
+  profileImage: string;
+
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    // setup profile image from gravatar
+    var user = this.authService.getUser();
+    var hashedEmail = Md5.hashStr(user.email.toLowerCase());
+    this.profileImage = `https://www.gravatar.com/avatar/${hashedEmail}?d=mp`;
   }
 
   logout(): void {
