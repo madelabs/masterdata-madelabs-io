7dd8f21337686284e28aa73f70939890edc877b0
nicholasbarger <nicholas@nicholasbarger.com>
2018-11-05 20:15:53 -0600

    Updated env files for prod and staging cognito clients

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   api: 'https://api.madelabs.io',
-  client_id: '',
+  client_id: '75m1gtceorsm9dbdhosol8a025',
   identity_url: 'https://made.auth.us-east-2.amazoncognito.com',
   ga: null,
-  redirect_uri: 'http://localhost:8080/callback' 
+  redirect_uri: 'https://enterprise.madelabs.io/callback' 
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   api: 'https://staging-api.madelabs.io',
-  client_id: '',
+  client_id: '124dn5bques8dgg65q7ggoredn',
   identity_url: 'https://made.auth.us-east-2.amazoncognito.com',
   ga: null,
-  redirect_uri: 'http://localhost:8080/callback' 
+  redirect_uri: 'https://staging-enterprise.madelabs.io/callback' 
 };
